eaa1c6ace6cbd9b3bb2d324dbc87c9a5ffc09223
mayurwagh <mayurwagh1412@gmail.com>
2026-06-04 15:04:47 +0530

    codeupdated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("welcome to NGP");
+    res.send("wele to NGP");
 });
 
 app.get("/health", (req, res) => {
